fbc81ffbc2127a042196ff6606a524dc98f109b6
Adrian Lopez <noknows6@gmail.com>
2019-11-24 18:25:48 -0600

    save new configuration express

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var exphbs = require('express-handlebars');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine('.hbs',exphbs({
+	extname: '.hbs',
+	defaultView: 'default',
+  	layoutsDir: __dirname + '/views/pages/',
+  	partialsDir: __dirname + '/views/partials/'
+
+}))
+app.set('view engine', '.hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('pages/presentation/home', { 
+  			title: 'Express',
+  			css :[
+  				'/stylesheets/presentation/home.css'  			
+  			],
+  			jsExternal : [
+  				'https://unpkg.com/react@16/umd/react.production.min.js',
+  				'https://unpkg.com/react-dom@16/umd/react-dom.production.min.js'
+  			] 
+  		});
 });
 
 module.exports = router;
